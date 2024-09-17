134d896964c2672f32444904dfa2846534cde1bb
kenmoh <kenneth.aremoh@gmail.com>
2024-09-17 22:37:38 +0100

    fix next deployment error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const About = () => {
     return (
         <div className="min-h-screen p-5 md:p-10 w-full bg-gradient-to-br from-gray-900 via-gray-800 to-orange-900">
-            <section className="w-full md:w-3/5 ">
-                <div className="items-center justify-center mx-auto">
+            <section className="w-full md:w-3/5 mx-auto">
+                <div className="items-center justify-center ">
                     <h2 className="text-white mb-3 text-3xl text-center font-extrabold">About Us</h2>
                     <p className="text-sm md:text-lg text-gray-300 ">
                         {welcome}
@@ -15,7 +15,7 @@ const About = () => {
                         {serviPal}
                     </p>
                 </div>
-                <div className="  items-center justify-center mx-auto">
+                <div className="  items-center justify-center ">
                     <h2 className="text-white my-3 text-2xl text-center font-semibold">Our Mission</h2>
                     <p className="text-sm md:text-lg text-gray-300">
                         {mission}
@@ -23,7 +23,7 @@ const About = () => {
 
                 </div>
 
-                <div className=" items-center justify-center mx-auto">
+                <div className=" items-center justify-center ">
                     <h2 className="text-white my-3 text-2xl text-center font-semibold">What we offer</h2>
                     {offers.map(offer => (
                         <div key={offer.label} className="my-3">
